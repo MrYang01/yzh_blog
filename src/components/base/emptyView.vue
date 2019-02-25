e860999d4951ecbd4afc194888353c4fd52e8084
yangzehong <yangzehong@jd.com>
2019-02-25 14:37:27 +0800

    init

---
+++ src/components/base/emptyView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div style='height:300px;' >
+            <!--class="flex-box middle"-->
+            <div>
+                <!--class="tc"-->
+                <!--<img src="~static/img/anyi.svg" alt="" style='height:80px;'>-->
+                <br/>
+                <p >{{empty}}</p>
+                <!--class="color-muted"-->
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        props: {
+            empty: {
+                type: String,
+                default: '暂无数据'
+            }
+        },
+        data() {
+            return {}
+        }
+    }
+
+</script>
+
+<style scoped>
+
+
+</style>
